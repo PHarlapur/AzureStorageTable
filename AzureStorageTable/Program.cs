f6fa2ee757fab511222567a425c6a61182344367
Prabhas Harlapur <prabhas.harlapur@gmail.com>
2017-06-07 06:05:05 +0000

    AzureTraining

---
+++ AzureStorageTable/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.WindowsAzure;
+using System.Configuration;
+
+
+namespace AzureStorageTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+
+            //CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            //CloudTable table = tableClient.GetTableReference("customers");
+            //table.CreateIfNotExists();
+
+
+           
+
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable table = tableClient.GetTableReference("orders");
+            table.CreateIfNotExists();
+            OrderEntity newOrder = new OrderEntity("Archer", "20160119");
+            newOrder.OrderNumber = "101";
+            newOrder.Status = "shipped";
+            TableOperation insertOperation = TableOperation.Insert(newOrder);
+            table.Execute(insertOperation);
+
+
+        }
+    }
+}
